6dd5cf26851eb732859a61f5c7fa153771dadc11
kuldeep1984 <kuldeep999p@gmail.com>
2014-02-18 23:01:34 +0530

    fnish layout and routs

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 DemoApp::Application.routes.draw do
+  get "users/new"
   get "static_pages/home"
   get "static_pages/help"
+  get "users/new"
+
+  root  'static_pages#home'
+  match '/signup',  to: 'users#new',            via: 'get'
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+  
   resources :microposts
 
   resources :users
