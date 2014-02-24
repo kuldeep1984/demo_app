9e18795a9b926cab32842c655f7ad2df1f616d83
kuldeep1984 <kuldeep999p@gmail.com>
2014-02-24 15:44:15 +0530

    sign-in-out fixes

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+  
+  def create
+    #render 'new'
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      sign_user user
+      redirect_to user
+    else
+      # Create aan error message
+      flash.now[:error] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+  
+  def new
+    
+  end
+  
+  def destroy
+    sign_out
+    redirect_to root_url
+  end
+end
